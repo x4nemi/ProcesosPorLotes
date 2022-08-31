ef1b02cfbf95547dac166f02665b967f22e38b8d
KevinLimon <kevin.limon7482@alumnos.udg.mx>
2022-08-30 22:11:08 -0500

    Form4 Prueba

---
+++ ProcesosPorLotes/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcesosPorLotes
+{
+    public class Class1
+    {
+
+        public Boolean BuscarSiExiste(int[]arreglo, int num)
+        {
+            Boolean respuesta = false;
+            for(int i = 0; i<arreglo.Length; i++)
+            {
+                if(arreglo[i] == num)
+                {
+                    respuesta = true;
+                }
+            }
+
+            return respuesta;
+        }
+    }
+}
---
+++ ProcesosPorLotes/Form4.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProcesosPorLotes
+{
+    public partial class Form4 : Form
+    {
+        AlmacenProcesos<Procesos> cola2 = new AlmacenProcesos<Procesos>();
+        public Form4(AlmacenProcesos<Procesos> cola)
+        {
+            InitializeComponent();
+            cola2 = cola;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tabla.Rows.Add(cola2.Regresar().Id, cola2.Regresar().Nombre, cola2.Regresar().Num1, cola2.Regresar().Num2, cola2.Regresar().Operacion, cola2.Regresar().Tiempo, cola2.Regresar().Numpro);
+            cola2.Ejecutar();
+        }
+    }
+}
